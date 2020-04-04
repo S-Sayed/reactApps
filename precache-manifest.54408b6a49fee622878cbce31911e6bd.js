be63c6a4b585e3a4f74a8eb0f9d7dd47a1abb90a
Sameh Elsayed <ssayed7190@gmail.com>
2020-04-04 20:26:35 +0400

    Updates

---
+++ precache-manifest.54408b6a49fee622878cbce31911e6bd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eba12081c63aaebc967897f7fa53dbe0",
+    "url": "/reactApps/index.html"
+  },
+  {
+    "revision": "c2830715082018b362ec",
+    "url": "/reactApps/static/css/main.7c61a155.chunk.css"
+  },
+  {
+    "revision": "9d16075b331f00cec587",
+    "url": "/reactApps/static/js/2.45b82879.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactApps/static/js/2.45b82879.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2830715082018b362ec",
+    "url": "/reactApps/static/js/main.38735cf9.chunk.js"
+  },
+  {
+    "revision": "7426987b127cb47ede2e",
+    "url": "/reactApps/static/js/runtime-main.326c756f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reactApps/static/media/logo.5d5d9eef.svg"
+  }
+]);
